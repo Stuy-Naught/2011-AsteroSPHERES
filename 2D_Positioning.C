0c8faf8b877610c32659eaabc954d8713379cdc0
Zachary Zee <Oneman2feet@gmail.com>
2011-09-17 13:47:53 -0400

    expanded framework to allow for easy destination change

--- 2D_Positioning.C
+++ 2D_Positioning.C
@@ -8,16 +8,63 @@
 //User01: guest1 Team: Stuy-rules! Project: 2D_Positioning
 void ZRUser01(float *myState, float *otherState, float time)
 {
-//sphere1 to laser1
-//spawn at (0.4, -0.6)
-//laser1 at (0.4, 0.0)
+//
+//SphereTarget[3] can be changed to make a different destination.
+//
  
-float laser1P[3] = {0.4, 0.0, 0.0};
-ZRSetPositionTarget(laser1P);
+/*
  
-DEBUG(("time is %f\n fuel is %f\n distance to laser is%f\n",
+SPHERE1 spawn at
+[0.4, -0.6]
+[0.4, -0.6, 0.0]
+ 
+Disruptor Upgrade
+[0.0,0.2]
+[0.0,0.0,-0.4]
+ 
+Shield
+[0.0,0.4]
+[0.0,0.0,0.4]
+ 
+Laser 1
+[0.4,0.0]
+[0.4,0.0,0.0]
+ 
+Laser 2
+[-0.4,0.0]
+[-0.4,0.0,0.0]
+ 
+Indigens
+[0.0,0.6]
+[0.0, 0.35, 0.2]
+ 
+Opulens
+[0.0,-0.6]
+[0.0,-0.35, -0.2]
+ 
+Mining Station 1
+[0.6,0.0]
+[0.6,0.0,-0.5]
+ 
+Mining Station 2
+[-0.6,0.0]
+[0.6,0.0,0.5]
+ 
+*/
+ 
+float laser1[3] = {0.4, 0.0, 0.0};
+ 
+float SphereTarget[3] = {0.0, 0.0, 0.0};
+ 
+SphereTarget[0]= laser1[0];
+SphereTarget[1]= laser1[1];
+SphereTarget[2]= laser1[2];
+ 
+ZRSetPositionTarget(SphereTarget);
+ 
+DEBUG(("time is %f\n fuel is %f\n distance to target is%f\n",
 time, getPercentFuelRemaining(),
-sqrt(mathSquare(myState[0]-0.4) + mathSquare(myState[1]))));
+sqrt(mathSquare(myState[0]-SphereTarget[0]) + mathSquare(myState[1]-SphereTarget[1]) + mathSquare(myState[2]-SphereTarget[2]))));
  
  
 }
